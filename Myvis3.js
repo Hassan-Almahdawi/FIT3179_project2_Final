e3d5db6c87fcaac97e4b2255dfa5ff067c0e9119
Hassan-Almahdawi <111483725+Hassan-Almahdawi@users.noreply.github.com>
2022-10-17 23:21:55 +1100

    Update Myvis3.js

--- Myvis3.js
+++ Myvis3.js
@@ -1,3 +1,2 @@
 var Myvis3 = "Tsunami_plot3.vg.json";
-vegaEmbed("#Tsunami_plot3", Myvis3);
-vegaEmbed("#Tsunami_plot3", chart, {"actions": false})
+vegaEmbed("#Tsunami_plot3", Myvis3,{"actions": false});
