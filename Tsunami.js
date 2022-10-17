1728019abbacbb05f381a5c58584da907a6d3d80
Hassan-Almahdawi <111483725+Hassan-Almahdawi@users.noreply.github.com>
2022-10-17 23:19:18 +1100

    Update Tsunami.js

--- Tsunami.js
+++ Tsunami.js
@@ -1,5 +1,5 @@
 var Myvis = "Tsunami.vg.json";
-vegaEmbed("#Tsunami", Myvis);
+vegaEmbed("#Tsunami", Myvis,{"actions": false});
 // .then(function(result) {
 // // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 // }).catch(console.error);
